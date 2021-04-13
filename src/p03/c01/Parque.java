3e5ea3e6e164904612ccff7a3ff41f9d65feb4c7
Usuario <Usuario@LAPTOP-DBQC53CB>
2021-04-13 13:17:59 +0200

    Metodo salirDelParque terminado

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -3,6 +3,14 @@ package src.p03.c01;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
+/**
+ * 
+ * @author Guillermo Alcuaz Temiño
+ * @author Pablo Marcos Bravo
+ * @since JDK 11
+ * @version 2.0
+ */
+
 public class Parque implements IParque{
 
 
@@ -42,10 +50,27 @@ public class Parque implements IParque{
 		// TODO
 		
 	}
-	
-	// 
-	// TODO MÃ©todo salirDelParque
-	//
+	public synchronized void salirDelParque(String puerta) {
+
+		try {
+			comprobarAntesDeEntrar();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		if (contadoresPersonasPuerta.get(puerta) == null) {
+			contadoresPersonasPuerta.put(puerta, 0);
+		}
+		
+		contadorPersonasTotales--;
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) - 1);
+
+		imprimirInfo(puerta, "Salida");
+		
+		checkInvariante();
+		
+		notify();
+	}
 	
 	
 	private void imprimirInfo (String puerta, String movimiento){
@@ -80,11 +105,9 @@ public class Parque implements IParque{
 		//
 	}
 
-	protected void comprobarAntesDeSalir(){		// TODO
-		//
-		// TODO
-		//
+	protected void comprobarAntesDeSalir() throws InterruptedException{		// TODO
+		while(contadorPersonasTotales == 0) {
+			wait();
+		}
 	}
-
-
 }
